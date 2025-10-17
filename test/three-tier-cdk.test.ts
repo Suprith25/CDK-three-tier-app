df1c377593cfc11aa8b2eeed0efe82eae197ba9f
Suprith <gowdajsuprith44@github.com>
2025-10-17 16:45:32 +0530

    small changes

--- test/three-tier-cdk.test.ts
+++ test/three-tier-cdk.test.ts
@@ -2,7 +2,7 @@
 // import { Template } from 'aws-cdk-lib/assertions';
 // import * as ThreeTierCdk from '../lib/three-tier-cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
+// example test. To run these tests, uncomment this file along with the code
 // example resource in lib/three-tier-cdk-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
